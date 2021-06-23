f6235ac68c165178eb956d638b42add481b3fe02
Alexandra Soares <al.soares@axulus.io>
2021-06-23 15:06:04 -0300

    hook de autenticação

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,19 @@
-import { useContext } from 'react';
 import { useHistory } from 'react-router-dom';
-import { auth, firebase } from '../services/firebase';
 
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
+import '../styles/auth.scss'
 
 // Componentes
 
 import { Button } from '../components/Button';
+import { useAuth } from '../hooks/useAuth';
 
-import '../styles/auth.scss'
-import { AuthContext } from '../App';
 
 export function Home() {
     const history = useHistory();
-    const { user, signInWithGoogle } = useContext(AuthContext);
+    const { user, signInWithGoogle } = useAuth();
 
     async function handleCreateRoom() {
         if (!user) {
