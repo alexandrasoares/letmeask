aa3d2ee024b2a75d9207cb0de1458f75da10e077
Alexandra Soares <al.soares@axulus.io>
2021-07-07 18:51:17 -0300

    fluxo de perguntas e estilização

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,5 +1,12 @@
+// IMPORTS REACT
+import { FormEvent, useState } from 'react';
+
 // IMPORTS COMPONENTS
 import { Button } from '../components/Button';
+import { RoomCode } from '../components/RoomCode';
+
+// IMPORTS HOOKS
+import { useAuth } from '../hooks/useAuth';
 
 // IMPORTS IMG
 import logoImg from '../assets/images/logo.svg'
@@ -8,20 +15,50 @@ import logoImg from '../assets/images/logo.svg'
 import { useParams } from 'react-router-dom';
 
 import '../styles/pages-style/room.scss'
-import { RoomCode } from '../components/RoomCode';
+import { database } from '../services/firebase';
 
 type RoomParams = {
     id: string;
 }
 
 export function Room() {
+    const { user } = useAuth();
     const params = useParams<RoomParams>();
+    const roomId = params.id;
+    const [newQuestion, setNewQuestion] = useState('');
+
+    async function handleSendQuestion(event: FormEvent) {
+        event.preventDefault();
+
+        if(newQuestion.trim() === '') {
+            return;
+        }
+
+        // react hot toast
+        if (!user) {
+            throw new Error('You must be logged in');
+        }
+
+        const question = {
+            content: newQuestion,
+            author: {
+                name: user.name,
+                avatar: user.avatar,
+            },
+            isHighLighted: false, // destaque do admin para determinar que essa pergunta esta sendo respondida atualmente.
+            isAnswared: false // se ja foi respondida ou nao
+        };
+
+        await database.ref(`rooms/${roomId}/questions`).push(question);
+
+        setNewQuestion(''); // substitui o valor de newquestion para vazio e limpa o textarea após o usuario enviar a pergunta
+    }
     return (
         <div id="page-room">
             <header>
                 <div className="content">
                     <img src={logoImg} alt="" />
-                    <RoomCode code={params.id}/>        
+                    <RoomCode code={roomId}/>        
                 </div>
             </header>
 
@@ -31,11 +68,24 @@ export function Room() {
                     <span> 4 Perguntas </span>
                 </div>
 
-                <form>
-                    <textarea placeholder="O que você quer perguntar?"/>   
+                <form onSubmit={handleSendQuestion}>
+                    <textarea 
+                        placeholder="O que você quer perguntar?"
+                        onChange={event => setNewQuestion(event.target.value)}
+                        value={newQuestion}
+                    />   
                     <div className="form-footer">
-                        <span> Para enviar uma pergunta <button> Faça seu login </button>.</span>
-                        <Button type="submit"> Enviar pergunta</Button>
+                        { user ? 
+                            ( 
+                                <div className="user-info">
+                                    <img src={user.avatar} alt={user.name} />
+                                    <span> {user.name} </span>
+                                </div> 
+                            ) 
+                            : 
+                            (<span> Para enviar uma pergunta <button> Faça seu login </button>.</span>) }
+                        
+                        <Button type="submit" disabled={!user}> Enviar pergunta</Button>
                     </div>
                 </form>                
             </main>
