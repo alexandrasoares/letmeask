c5bce496c75fb195b7698c6875fe64e2077789c7
Alexandra Soares <al.soares@axulus.io>
2021-07-07 00:31:32 -0300

    exibindo o valor do id como codigo da sala

--- src/components/RoomCode.tsx
+++ src/components/RoomCode.tsx
@@ -1,14 +1,21 @@
 import copyImg from '../assets/images/copy.svg';
 import '../styles/componentes-style/room-code.scss';
 
+type RoomCodeProps = {
+    code: string;
+}
 
-export function RoomCode() {
+export function RoomCode(props: RoomCodeProps) {
+
+    function copuRoomCodeToClipBoard() {
+        navigator.clipboard.writeText(props.code)
+    }
     return (
-        <button className="room-code">
+        <button className="room-code" onClick={copuRoomCodeToClipBoard}>
             <div>
                 <img src={copyImg} alt="Copy room code" />
             </div>
-            <span>Sala 289128019281938732 </span>
+            <span>Sala #{props.code} </span>
         </button>
     )
 }
--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -5,16 +5,23 @@ import { Button } from '../components/Button';
 import logoImg from '../assets/images/logo.svg'
 
 // STYLE
+import { useParams } from 'react-router-dom';
+
 import '../styles/pages-style/room.scss'
 import { RoomCode } from '../components/RoomCode';
 
+type RoomParams = {
+    id: string;
+}
+
 export function Room() {
+    const params = useParams<RoomParams>();
     return (
         <div id="page-room">
             <header>
                 <div className="content">
                     <img src={logoImg} alt="" />
-                    <RoomCode/>        
+                    <RoomCode code={params.id}/>        
                 </div>
             </header>
 
